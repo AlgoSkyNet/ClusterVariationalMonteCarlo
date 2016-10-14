79deabde3a46d3f354f9962db5a74d049b2cd1ac
Chris Dahnken <dahnken@gmx.net>
2016-10-14 09:05:16 +0200

    fixing descriptions, moving configurations

--- include/vectors/mcAbstractVector.h
+++ include/vectors/mcAbstractVector.h
@@ -1,6 +1,6 @@
 /* 
  * File:   MCAbstractVector.h
- * Author: chris
+ * Author: Chris Dahnken 
  *
  * Created on August 3, 2011, 11:14 AM
  */
@@ -12,12 +12,20 @@
 #define	MCABSTRACTVECTOR_H
 using namespace std;
 
+/* 
+ * mcAbstractVector 
+ * Defines a call with just two functions
+ * getDimension1: returns the length of the vectors
+ * get(i): returns the element i of the vectors 
+ * 
+ * Additional there is a print function for debugging purposes
+*/
 class mcAbstractVector {
 private:
 protected:
     mc_state dimension1;
 public:
-
+// Return the length of the vector
     virtual mc_state getDimension1() {
         return dimension1;
     }
@@ -25,6 +33,7 @@ public:
 //#ifndef USE_64BIT
 //    virtual double get(mc_uint64 i) = 0;
 //#endif
+    // Return the element i of the vector
     virtual double get(mc_state i) = 0;
 
     void print() {
